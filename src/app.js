ece5a2615fc94b86d360153dfd97fda2a7aeb12e
soyeon <soylee57@naver.com>
2023-02-17 20:44:43 +0900

    함수 Component_upgrade

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* @jsx createElement */
-import {createDom, createElement, render} from './react'; 
+import {createElement, render} from './react'; 
 
 // 함수 component 규칙
 // 함수 component는 대문자로 시작해야 한다.
@@ -9,17 +9,24 @@ import {createDom, createElement, render} from './react';
 // => 함수로 구별되면 createElement()는 문자열로 받지 않는다.
 // => 함수의 리턴값 jsx 문법이 createElement() 함수 호출 구문으로 변환되고 결과적으로 createElement() 함수가 리턴한 객체 값을 리턴한다.
 
-function Title() {
-  return <h1>React 잘 만들기</h1>;
+// 함수 component는 1번쨰 인자로 props를 받는다. props로 전달된 데이터를 활용한다.
+// <Title>React 잘 만들기</Title>의 'React 잘 만들기'는 component의 children으로 넘기고 싶다.
+// props.children은 문자열이 아닌 자바스크립트 코드로 인식시켜 주기위해 Jsx에서는 Brace로 구분해준다.
+
+function Title(props) {
+  return <h1>{ props.children }</h1>;
+}
+
+function Item(props) {
+  return <li style={`color:${ props.color }`}>{ props.children }</li>;
 }
 
 const vdom = <section>
-  <Title></Title>
-  <h1>React 만들기</h1>
+  <Title>React 만들기</Title>
   <ul>
-    <li style="color:red">첫 번째 아이템</li>
-    <li>두 번째 아이템</li>
-    <li>세 번째 아이템</li>
+    <Item color="pink">첫 번째 아이템</Item>
+    <Item color="skyblue">두 번째 아이템</Item>
+    <Item>세 번째 아이템</Item>
   </ul>
 </section>
 
--- src/react.js
+++ src/react.js
@@ -22,9 +22,23 @@ export function createElement(tag, props, ...children) {
   props = props || {};
 
   // tag가 함수인지 문자열인지 구분하기
-  // tag의 앞글자가 대문자라면 함수 라는 규칙을 가진다. (실제 React 규칙과 같도록 작성해보기)
+  // 실제로 tag의 앞글자가 대문자라면 함수로 인식하는 규칙을 가진다.
   if (typeof tag === 'function') {
-    return tag(); //함수가 리턴하는 jsx값이 ->  createElement() 함수 호출로 변환되고 -> createElement() 반환 값이 리턴된다.
+    // 함수에 전달된 props에 childrne 이 들어오면 props에 children이라는 key를 만들어서 거기다가 넣어 주는 식으로 react처럼 만들어본다.
+    // 만들어진 vdom 객체의 구조를 보면 children은 3번째 인자로 들어오는데 
+    // 함수 component에서 props를 1번째 인자로 children을 2번째 인자로 따로 구분해서 받도록 하지 않고 
+    // props로 받아서 props.children으로 받게끔 디자인 했다. 이건 특별한 코드라기 보다는 react팀의 디자인이라고 볼 수 있다. 
+    // children이 하나 이면 단순 값으로 받고 둘 이상이면 배열로 받는다. 
+    if (children.length > 0) {
+      return tag({
+        ...props,
+        children: children.length === 1 ? children[0] : children,
+      })
+    } else {
+      // 함수가 리턴하는 jsx값이 ->  createElement() 함수 호출로 변환되고 -> 결과적으로는 createElement() 반환 값(객체)이 리턴된다.
+      return tag(props); 
+    }
+
   } else {
     return {tag, props, children} ;
   }
