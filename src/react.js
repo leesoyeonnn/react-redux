ffa99fb824251b44a464ad02f123ccec19a4d1b2
soyeon <soylee57@naver.com>
2023-02-20 13:10:41 +0900

    Hook 맥락 이해하기

--- src/react.js
+++ src/react.js
@@ -1,3 +1,6 @@
+const hooks = []; //Hook은 순서가 필요
+let currentComponent = 0; //순서를 제어하기 위한 변수 
+
 export class Component {
   constructor(props) {
     this.props = props;
@@ -28,59 +31,50 @@ function makeProps(props, children) {
   };
 }
 
+function useState(initValue) { //useState는 1번째 인자로 항상 초기값을 받는다.
+  //useState()가 실행되면 hooks에 값을 저장해야 한다. 그 값을 안쪽에서 저장하면 안된다. 유지해야해서 바깥쪽에 hooks를 만들어두었다. 
+
+  let position = currentComponent - 1;
+
+  // 기존 값이 있으면 초기값을 넣으면 안된다. 
+  if(!hooks[position]) {//hooks에 값이 없으면
+    hooks[position] = initValue;
+  }
+
+  //값을 바꾸는 함수
+  //hooks의 값은 바깥쪽(component쪽)에서 접근할 수 없기 때문에 modifier에 통해서만 수정될 수 있는 구조
+  const modifier = nextValue => {
+    hooks[position] = nextValue;
+  }
+
+  return [ hooks[position], modifier]
+}
+
 export function createElement(tag, props, ...children) {
   props = props || {};
 
   if (typeof tag === 'function') {
-
     if (tag.prototype instanceof Component) {
-
       const instance = new tag(makeProps(props, children));
       return instance.render();
-    
     } else {
+
+      hooks[currentComponent] = null; //초기 세팅
+      //createElement()가 한 번 실행될 때마다 component 하나가 만들어진다.
+      //함수 호출 후에는 그 다음 component가 만들어져야 하니 currentComponent 증가
+      currentComponent++;
       
-      if (children.length > 0) {
+      if (children.length > 0) { // 함수 component가 호출되는 지점
         return tag(makeProps(props, children));
       } else {
         return tag(props); 
       }
     }
-  } else {
-    return {tag, props, children} ;
   }
+  
+  return {tag, props, children} ;
 }
 
-// render 함수는 늘 새롭게 업데이트 되는 경우에 호출될테니 vdom은 늘 새롭게 실제 DOM에 적용할 객체다.
 export function render(vdom, container) {
     container.appendChild(createDom(vdom))
 }
-
-export const render = (function() { 
-  let prevDom = null;
-
-  return function(vdom, container) { // closer를 만들기 위해서 즉시 새로운 함수를 return 한다.
-    // prevDom이 closer에 갇혀서 바깥에서는 참조할 수 없고 내부적으로 매번 업데이트될 때마다 비교할 수 있는 logic을 삽입할 수 있는 구조가 만들어진다. 
-    if(prevDom === null) {
-      prevDom = vdom;
-    }
-
-    // diff (비교) 과정 생략
-    // prevDom과 vdom의 객체 수준에서 비교해서 변경 사항만 업데이트된 새로운 객체를 만든 후 
-    // 변경 사항 부분만 createDom에 전달한다.
-    // createDom은 새로운 DOM을 만드는 함수라서 실제로는 새로운 DOM을 만드는 함수가 아닌 업데이트되는 함수에 전달한다.
-
-    container.appendChild(createDom(vdom))
-  }
-})();
-// 즉시 실행 함수를 한번 더 호출해서 내부 함수를 render에 넣어주면 
-// return function(vdom, container) {
-//   if(prevDom === null) {
-//     prevDom = vdom;
-//   }
-
-//   // diff
-
-//   container.appendChild(createDom(vdom))
-// }
-// 이 부분이 바깥으로 내보내진다. (export 해줘야함)
